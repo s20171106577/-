546bd14a6c6cd8ee7bc0fa404001aebbce129aca
s20171106577 <s20171106577@123.local>
2018-10-27 09:40:45 +0800

    Initial Commit

---
+++ 10.17Tests/_0_17Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _0_17Tests.swift
+//  10.17Tests
+//
+//  Created by s20171106577 on 2018/10/27.
+//  Copyright © 2018 s20171106577. All rights reserved.
+//
+
+import XCTest
+@testable import _0_17
+
+class _0_17Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
